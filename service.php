5f365773bdddbc4ebf835abb20f07a3d79691a6b
glicha <gabriel.licha@gmail.com>
2016-11-23 03:17:11 -0300

    Version v0.1

--- service.php
+++ service.php
@@ -51,6 +51,7 @@ function RetrieveUserProfile($idFacebook){
 
     return($profile);
 }
+
 if ( !isset( $HTTP_RAW_POST_DATA ) ) $HTTP_RAW_POST_DATA =file_get_contents( 'php://input' );
 $server->service($HTTP_RAW_POST_DATA);
 ?>
